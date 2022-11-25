d29258bca7cdd0a6db5a40859811bb7688046864
Dawn Walker <nosirrah.reklaw@gmail.com>
2022-11-25 17:01:50 +1100

    a

--- packages/babel-preset-dawnie/webpack-overrides.mjs
+++ packages/babel-preset-dawnie/webpack-overrides.mjs
@@ -9,14 +9,15 @@ const webpackOverride = function () {
 		// This function transforms the Babel configuration on a per-file basis
 		/**
 		 * @param {{ options: { caller: any; }; }} config
+		 * @param source
 		 */
 		config(config, { source }) {
 			// Babel Macros are notoriously hard to cache, so they shouldn't be
 			// https://github.com/babel/babel/issues/8497
 			// We naively detect macros using their package suffix and add a random token
 			// to the caller, a valid option accepted by Babel, to compose a one-time
-			// cacheIdentifier for the file. We cannot tune the loader options on a per
-			// file basis.
+			// cacheIdentifier for the file. We cannot tune the loader options on a
+			// per-file basis.
 			if (macroCheck.test(source)) {
 				return Object.assign({}, config.options, {
 					caller: Object.assign({}, config.options.caller, {
