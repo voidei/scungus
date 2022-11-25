a61503d89593b1415d4c9cf837b2fc7bfeb471a5
Dawn Walker <nosirrah.reklaw@gmail.com>
2022-11-25 12:54:52 +1100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const process = require('process');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.tsx',
+	resolve: {
+		extensions: ['.js', '.jsx', '.ts', '.tsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx|ts|tsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					// `.swcrc` can be used to configure swc
+					loader: 'swc-loader',
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(jpe?g|gif|png|svg)/,
+				type: 'asset/resource',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'public/index.html'),
+		}),
+	],
+	devServer: {
+		//contentBase: path.join(__dirname, "dist"),
+		port: process.env.PORT || 3000,
+		liveReload: true,
+	},
+};
