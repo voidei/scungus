48d43be02014f42876284ce194fe1b7eb28d5bc1
Dawn Walker <nosirrah.reklaw@gmail.com>
2022-11-26 14:53:19 +1100

    my fucking around
    likely to delete these later

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,25 @@
-const Button = function () { }
+import * as material from '@mui/material';
 
-export default Button;
+let scungus = 0;
+
+const pressButton = function () {
+	scungus++;
+	console.log(scungus);
+};
+
+export function Button() {
+	return (
+		<div className="btn">
+			<material.Button
+				onClick={pressButton}
+				variant="outlined"
+				color="success"
+				size="large"
+			>
+				scungus:)
+			</material.Button>
+			<br />
+			<div className="text scungus">scungus = {scungus.toString()}</div>
+		</div>
+	);
+}
---
+++ src/components/clock.tsx
@@ -0,0 +1,8 @@
+import { pressButton } from '../components/clock';
+
+export const pressButton = function () {
+	let amount = 0;
+	amount++;
+	console.log(amount);
+	return amount;
+};
