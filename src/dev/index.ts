8a93347af8178faab5ae4add194df27eb95a05aa
Dawn Walker <nosirrah.reklaw@gmail.com>
2022-11-26 14:52:38 +1100

    removed that react buddy thing

--- src/dev/index.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useInitial } from './useInitial';
-
-const ComponentPreviews = React.lazy(() => import('./previews.tsx'));
-
-export {
-	ComponentPreviews,
-	useInitial
-};
