64de160c823dd12180f9499c4ed8d889c4ce587e
Dawn Walker <dawniepieuwu@gmail.com>
2024-01-23 15:30:02 +1100

    Create SaveScungus.tsx

---
+++ src/hooks/SaveScungus.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@mui/material';
+import { storeScungusLocally } from '../functions/StoreScungus';
+
+const SaveScungus = () => {
+  const [
+    amount,
+    setAmount
+  ] = storeScungusLocally('scungus', 0);
+  const save = () => setAmount(amount);
+
+  return (
+    <div>
+      <div>
+        <Button
+        variant='contained'
+        color='secondary'
+        type='submit'
+        onClick={save}
+        aria-label='scungus:) save'
+        className='button save'>
+          Save Game
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default SaveScungus;
